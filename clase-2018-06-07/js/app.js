73e53e6e8bfef435bcb1d8a56e57087cc1107ce6
MatiasOrlandini <maty2327@hotmail.com>
2018-06-07 11:15:04 -0300

    project refactor

---
+++ clase-2018-06-07/js/app.js
@@ -0,0 +1,11 @@
+console.log("gjgj");
+var Play = function () {
+  var cells = Board.getCells();
+  Board.updateCells(cells);
+
+}
+
+window.onload = function() {/* Se ejecuta luego de que se cargue todo el html */
+  var btnPlay = Board.getPlayButton();
+  btnPlay.onclick = Play;
+}
