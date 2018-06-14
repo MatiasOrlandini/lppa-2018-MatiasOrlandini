f51b58574be25d87bd33fc66ebc41f8135f13bf2
MatiasOrlandini <maty2327@hotmail.com>
2018-06-14 10:10:18 -0300

    Toggle Cell Alive

---
+++ clase-2018-06-14/js/app.js
@@ -0,0 +1,19 @@
+console.log("sdfsdfs");
+
+var play = function(){
+  var liAlives = Board.getCells();
+  Board.updateCells(liAlives);
+}
+
+var init = function(){
+  Board.createCells();
+  var cells = Board.getCells();
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].onclick = Cell.toggle;
+  }
+  // var btnPlay = Board.getPlayButton();
+  // btnPlay.onclick = play;
+}
+
+
+window.onload = init;
